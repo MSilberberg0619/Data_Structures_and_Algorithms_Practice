9791871b5aeccd930d36e9d534d5b21d403e9e07
Mark Silberberg <mark.silberberg20@gmail.com>
2019-10-30 13:51:07 -0500

    "Added Further Lessons from the Textbook"

--- Chapter_3/DS_A_Chapter_3.py
+++ Chapter_3/DS_A_Chapter_3.py
@@ -197,6 +197,36 @@ def infixToPostfix(infixexpr):
 #Note: having or not having spaces in the input to the infixToPostfix function can causes problems and should be
 #      modified. The function should take a general format, no matter how many white spaces
 
+#This function converts a postfix expression to an equivalent arithmetic outcome using a helper function doMath
+
+def postfixEval(postfixExpr):
+    operandStack = Stack()
+
+    tokenList = postfixExpr.split()
+
+    for token in tokenList:
+        if token in "0123456789":
+            operandStack.push(int(token))
+        else:
+            operand2 = operandStack.pop()
+            operand1 = operandStack.pop()
+            result = doMath(token, operand1, operand2)
+
+    return operandStack.pop()
+
+def doMath(op, op1, op2):
+    if op == "*":
+        return op1 * op2
+    elif op == "/":
+        return op1 / op2
+    elif op == "+":
+        return op1 + op2
+    else:
+        return op1 - op2
+
+#IMPORTANT: Both the postfix conversion and postfix evaluation programs assumed no input error --> they could use
+#           error detection in future versions
+
 if __name__ == "__main__":
 
     s = Stack()
